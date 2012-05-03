fd03434da3aeb5ac949a26e96961472d4d038796
Mathias Bogaert <mathias.bogaert@gmail.com>
2012-05-03 17:20:04 +0100

    Some minor fixes. Tested skype script.

--- src/test/java/com/zestia/rest/capsule/restapi/CapsuleTest.java
+++ src/test/java/com/zestia/rest/capsule/restapi/CapsuleTest.java
@@ -13,8 +13,8 @@ import java.util.Map;
 public abstract class CapsuleTest {
     public FakeApplication fakeApplication() {
         Map<String, String> capsuleConfig = ImmutableMap.of(
-                "capsulecrm.url", "https://pearsons.capsulecrm.com",
-                "capsulecrm.token", "e1d2dacc6dfa2213635c458beb86674c");
+                "capsulecrm.url", "https://YOUTS.capsulecrm.com",
+                "capsulecrm.token", "YOURS");
 
         return new FakeApplication(new java.io.File("."), Helpers.class.getClassLoader(), capsuleConfig, new ArrayList<String>());
     }
