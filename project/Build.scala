bc5f91586e39e011e8331f6f05173c96ff9126f8
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-05-29 19:53:00 +0200

    Release to repos.

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,6 @@ object Build extends sbt.Build {
 
   lazy val root = Project(id = "capsulecrm-java", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
-    name := "Capsule CRM API Java Client",
     organization := "uk.co.coen",
     resolvers += Resolver.typesafeRepo("releases"),
     scalaVersion := "2.10.1",
@@ -15,14 +14,25 @@ object Build extends sbt.Build {
     testOptions += Tests.Argument(TestFrameworks.JUnit, "-q", "-v"),
     libraryDependencies += "play" %% "play-java" % playVersion,
     libraryDependencies += "com.thoughtworks.xstream" % "xstream" % "1.4.4",
+    crossPaths := false,
 
-    // testing
+      // testing
     libraryDependencies += "play" %% "play-test" % playVersion % "test",
-    libraryDependencies += "org.specs2" %% "specs2" % "1.14",
+    libraryDependencies += "org.specs2" %% "specs2" % "1.14" % "test",
     libraryDependencies += "org.jsoup" % "jsoup" % "1.7.2" % "test",
     libraryDependencies += "com.novocode" % "junit-interface" % "0.10-M4" % "test",
 
-    pomExtra :=
+    publishMavenStyle := true,
+    publishTo <<= version {
+      v: String =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomExtra := (
+      <url>http://coenrecruitment.github.io/capsulecrm-java/</url>
       <licenses>
         <license>
           <name>Apache 2</name>
@@ -30,5 +40,17 @@ object Build extends sbt.Build {
           <distribution>repo</distribution>
         </license>
       </licenses>
+      <scm>
+        <url>git@github.com:coenrecruitment/capsulecrm-java.git</url>
+        <connection>scm:git:git@github.com:coenrecruitment/capsulecrm-java.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>analytically</id>
+          <name>Mathias Bogaert</name>
+          <url>http://coen.co.uk</url>
+        </developer>
+      </developers>
+      )
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,3 +3,5 @@ addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.4.0")
 addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.0")
 
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.3")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
