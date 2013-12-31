10b7b5b137fdc3f1d12c8967229bc6356d0aa3e9
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-12-31 12:16:26 +0000

    Fix test.

--- src/test/java/uk/co/coen/capsulecrm/client/HistoryTest.java
+++ src/test/java/uk/co/coen/capsulecrm/client/HistoryTest.java
@@ -6,19 +6,13 @@ import static org.fest.assertions.Assertions.assertThat;
 
 public class HistoryTest extends CapsuleTest {
     @Test
-    public void testEmptyHistory() throws Exception {
+    public void testHistory() throws Exception {
         CHistory history = person.listHistory().get();
         assertThat(history.size).isEqualTo(0);
 
-        deleteTestPerson();
-        assertThat(CPerson.listByEmailAddress(person.firstEmail().emailAddress).get()).hasSize(0);
-    }
-
-    @Test
-    public void testSingleItem() throws Exception {
         person.add(new CHistoryItem("test history item"));
 
-        CHistory history = person.listHistory().get();
+        history = person.listHistory().get();
         assertThat(history.size).isEqualTo(1);
 
         deleteTestPerson();
