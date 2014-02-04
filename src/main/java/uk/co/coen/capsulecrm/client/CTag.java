2279f7b2c5410f06840b01c1d96781d81bd53b2a
Mathias Bogaert <mathias.bogaert@gmail.com>
2014-02-04 13:51:36 +0100

    Add id to Tag (not in public docs).

--- src/main/java/uk/co/coen/capsulecrm/client/CTag.java
+++ src/main/java/uk/co/coen/capsulecrm/client/CTag.java
@@ -3,6 +3,7 @@ package uk.co.coen.capsulecrm.client;
 import com.google.common.base.Objects;
 
 public class CTag {
+    public String id;
     public String name;
 
     public CTag(String name) {
@@ -12,6 +13,7 @@ public class CTag {
     @Override
     public String toString() {
         return Objects.toStringHelper(this)
+                .add("id", id)
                 .add("name", name)
                 .toString();
     }
