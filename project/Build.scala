80bcb8b4243862fa00e30aa2e69d2c9e743ba29b
Mathias Bogaert <mathias.bogaert@gmail.com>
2014-05-05 07:14:23 +0100

    Minor version bumps. [ci skip]

--- project/Build.scala
+++ project/Build.scala
@@ -24,8 +24,8 @@ object Build extends sbt.Build {
         invoke(null, "ROOT")
     ),
 
-    libraryDependencies += "com.typesafe" % "config" % "1.2.0",
-    libraryDependencies += "com.google.guava" % "guava" % "16.0.1",
+    libraryDependencies += "com.typesafe" % "config" % "1.2.1",
+    libraryDependencies += "com.google.guava" % "guava" % "17.0",
     libraryDependencies += "joda-time" % "joda-time" % "2.3",
     libraryDependencies += "com.ning" % "async-http-client" % "1.8.8",
     libraryDependencies += "com.thoughtworks.xstream" % "xstream" % "1.4.7",
